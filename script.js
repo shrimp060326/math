c354eb3c41098c64ae346e284c12a2b0ae841172
shrimp060326 <93255337+shrimp060326@users.noreply.github.com>
2024-05-30 16:25:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var box5 = document.getElementById("box5");
-var box6 = document.getElementById("box6");
-var box7 = document.getElementById("box7");
-var box8 = document.getElementById("box8");
-var box9 = document.getElementById("box9");
-var box10 = document.getElementById("box10");
-var box11 = document.getElementById("box11");
-var box12 = document.getElementById("box12");
-var box13 = document.getElementById("box13");
-var box14 = document.getElementById("box14");
-var box15 = document.getElementById("box15");
-var box16 = document.getElementById("box16");
-var box17 = document.getElementById("box17");
-var box18 = document.getElementById("box18");
-var box19 = document.getElementById("box19");
-var box20 = document.getElementById("box20");
-var box21 = document.getElementById("box21");
-var box22 = document.getElementById("box22");
-var box23 = document.getElementById("box23");
-var box24 = document.getElementById("box24");
+var btn = document.getElementById("btn");
 
+var list = [];
+console.log(list);
 
+var randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
+
+function d(id) {
+    alert(list[0]);
+}
 function f(id) {
-    document.getElementById(id).style.backgroundColor = "red";
+    if(document.getElementById(id).style.backgroundColor == "red") {
+        document.getElementById(id).style.backgroundColor = "gray";
+        list.splice(list.indexOf(id),1);    
+    }
+    else {
+        document.getElementById(id).style.backgroundColor = "red";
+        list.push(id);
+    }
 }
